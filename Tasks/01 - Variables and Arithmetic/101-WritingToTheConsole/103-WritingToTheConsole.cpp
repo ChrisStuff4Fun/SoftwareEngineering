8632cf334c66a0841ce3d77358ba4aa978089fdc
Chris Catlin <c.catlin@yahoo.com>
2023-09-28 15:38:33 +0100

    Task 103 - Rounding
    Rounding and printing variables to console completed, use variable placeholder %(roundValue)variableType

--- Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
+++ Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
@@ -20,7 +20,9 @@ int main()
 
     // TASK(b) - Use printf to display the value of the variable area. Look at the lecture slides and find out how to limit this to 3 decimal places
     // Write solution here
-    
+
+    printf("Area = %4.3f \n", area);    // NOTES - Float after % placeholder signifies round to value e.g: round to 3 before, and 3 sigfigs after
+
     //
 
     // You can output complex strings to the terminal
@@ -30,6 +32,8 @@ int main()
 
     // TASK(c) - using printf, display the radius and the area using a single printf statement
     // Write solution here
+
+    printf("Area = %4.3f \nRadius = %3.0f", area, radius);
 
     //
 }

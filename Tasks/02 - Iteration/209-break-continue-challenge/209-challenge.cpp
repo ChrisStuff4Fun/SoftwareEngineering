62e50898393456d8ee69084888b1eaef1bf3fa21
Chris Catlin <c.catlin@yahoo.com>
2023-10-05 15:33:24 +0100

    Tasks completed

--- Tasks/02 - Iteration/209-break-continue-challenge/209-challenge.cpp
+++ Tasks/02 - Iteration/209-break-continue-challenge/209-challenge.cpp
@@ -3,18 +3,23 @@
 int main()
 {
     bool quitEarly = false;
-    for (int i = 0; i <= 10; i++)   // potentially loop from 1 to 10  
+    for (int i = 0; i >= 0; i++)   // potentially loop from 1 to 10  
     {
         char c = getchar(); //Read character
-        char _ = getchar(); //Read and discard newline (nice little hack :)
-        if (c == 'q')
+        char _ = getchar(); //Read and discard newline nice little hack :)
+        if (c == 'q' or c == 'Q')
         {
             printf("\tUser stopped the loop early at %d\n", i);
             quitEarly = true;       //Flag that indicated the loop was exited
             break;                  // break out of the for loop
         }
-        printf("Loop counter = %d\n", i);
-    }
+        else
+        {
+            printf("Loop counter = %d\n", i);
+            continue;
+
+        }
+    }       
 
     //Was the loop exited with a break?
     if (quitEarly == true) {
